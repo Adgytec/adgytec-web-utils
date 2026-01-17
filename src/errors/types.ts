ce23b75dfe615e98560fe1597aa5c9d07290d6c5
rohan <rohanverma031@gmail.com>
2026-01-17 18:59:18 +0530

    feat: added error code type

--- src/errors/types.ts
+++ src/errors/types.ts
@@ -10,3 +10,16 @@ export const APIErrorResponseSchema = z.object({
 export type APIErrorResponse = z.infer<typeof APIErrorResponseSchema>;
 
 export type NewApiErrorResponse = (res: unknown) => APIErrorResponse;
+
+export type ErrorCode =
+  | "server-error"
+  | "network-error"
+  | "too-many-requests-error"
+  | "authentication-error"
+  | "authorization-error"
+  | "not-found-error"
+  | "method-not-allowed-error"
+  | "form-field-error"
+  | "user-action-error"
+  | "content-too-large-error"
+  | "unknown-error";

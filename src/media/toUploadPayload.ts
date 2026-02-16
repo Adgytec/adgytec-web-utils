043962b847f1720838f53a780b937b959637be38
rohan <rohanverma031@gmail.com>
2026-02-16 10:48:46 +0530

    feat: added to upload payload helpers

---
+++ src/media/toUploadPayload.ts
@@ -0,0 +1,13 @@
+import type { ToUploadPayload, ToUploadPayloads } from "./types";
+
+export const toUploadPayload: ToUploadPayload = (item) => {
+  return {
+    id: item.id,
+    size: item.size,
+    name: item.name,
+  };
+};
+
+export const toUploadPayloads: ToUploadPayloads = (items) => {
+  return items.map(toUploadPayload);
+};
--- src/media/types/index.ts
+++ src/media/types/index.ts
@@ -1,1 +1,2 @@
 export * from "./mediaInfo";
+export * from "./toUploadPayload";

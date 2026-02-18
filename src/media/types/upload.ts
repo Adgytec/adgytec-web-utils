f962748552cb20c95bd519fb702540c505184067
rohan <rohanverma031@gmail.com>
2026-02-18 09:56:17 +0530

    feat: added type for retry task

--- src/media/types/upload.ts
+++ src/media/types/upload.ts
@@ -1,3 +1,4 @@
+import type { MultipartUtil } from "../multipartUtil";
 import type { LifecycleHandler } from "./lifecycle";
 import type { UploadDetails } from "./uploadDetails";
 
@@ -14,4 +15,40 @@ export type UploadItems = (
 export type MultipartUploadedPartDetails = {
   etag: string;
   partNumber: number;
+};
+
+type SinglepartUploadRetry = {
+  type: "singlepart-upload";
+  uploadURL: string;
+  blob: Blob;
+};
+
+type SinglepartCompleteRetry = {
+  type: "singlepart-complete";
+  successCallbackURL: string;
+};
+
+type MultipartPartUploadRetry = {
+  type: "multipart-part-upload";
+  uploadURL: string;
+  multipartObj: MultipartUtil;
+  blob: Blob;
+  startByte: number;
+  endByte: number;
+};
+
+type MultipartCompleteRetry = {
+  type: "multipart-complete";
+  successCallbackURL: string;
+  multipartObj: MultipartUtil;
+};
+
+type RetryAction =
+  | SinglepartUploadRetry
+  | SinglepartCompleteRetry
+  | MultipartPartUploadRetry
+  | MultipartCompleteRetry;
+
+export type Retry = RetryAction & {
+  retryCount: number;
 };

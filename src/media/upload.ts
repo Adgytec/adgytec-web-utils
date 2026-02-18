186eed07dd755097c3d064ddb801d708df788f35
rohan <rohanverma031@gmail.com>
2026-02-18 19:55:35 +0530

    refactor: added await to uploadItems call

--- src/media/upload.ts
+++ src/media/upload.ts
@@ -2,7 +2,7 @@ import type { UploadItems, UploadItem } from "./types";
 import { Upload } from "./uploadUtil";
 
 export const uploadItem: UploadItem = async (item, handler, limits) => {
-  uploadItems([item], handler, limits);
+  await uploadItems([item], handler, limits);
 };
 
 export const uploadItems: UploadItems = async (items, handler, limits) => {

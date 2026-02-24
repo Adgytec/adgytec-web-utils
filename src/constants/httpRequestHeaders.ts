38afced9eadaf3ed8fc8c9930723ea1f8f4dd3ed
rohan <rohanverma031@gmail.com>
2026-02-24 10:23:59 +0530

    feat: added custom header x-user-locale

--- src/constants/httpRequestHeaders.ts
+++ src/constants/httpRequestHeaders.ts
@@ -4,3 +4,5 @@ export const HTTP_HEADER_CONTENT_TYPE_APPLICATION_JSON = "application/json";
 export const HTTP_HEADER_AUTHORIZATION = "Authorization";
 export const HTTP_HEADER_AUTHORIZATION_BEARER_SCHEME = "Bearer";
 export const HTTP_HEADER_AUTHORIZATION_BASIC_SCHEME = "Basic";
+
+export const HTTP_HEADER_USER_LOCALE = "x-user-locale";

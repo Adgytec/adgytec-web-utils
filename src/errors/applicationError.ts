3ec4b904a1241317af122f95236bb4dba7015dcb
rohan <rohanverma031@gmail.com>
2026-03-12 14:51:47 +0530

    feat: added getters for values in application error

--- src/errors/applicationError.ts
+++ src/errors/applicationError.ts
@@ -1,13 +1,21 @@
 import { BaseError } from "./baseError";
 
 export class ApplicationError extends BaseError {
-    code: string;
-    res: unknown;
+    #code: string;
+    #details: unknown;
 
-    constructor(code: string, res: unknown) {
+    constructor(code: string, details: unknown) {
         super("application-error");
 
-        this.code = code;
-        this.res = res;
+        this.#code = code;
+        this.#details = details;
+    }
+
+    get details() {
+        return this.#details;
+    }
+
+    get code() {
+        return this.#code;
     }
 }

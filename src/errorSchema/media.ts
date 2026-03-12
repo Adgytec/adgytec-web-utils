158d9d438f832424aaaa86c6a1a3b911ed3a4f3a
rohan <rohanverma031@gmail.com>
2026-03-12 17:02:37 +0530

    feat: added schema for media errors

---
+++ src/errorSchema/media.ts
@@ -0,0 +1,60 @@
+import z from "zod";
+import {
+    INVALID_MULTIPART_NUMBER,
+    MEDIA_OBJECT_NOT_FOUND,
+    MEDIA_TOO_LARGE,
+    MEDIA_ITEMS_LIMIT_EXCEEDED,
+    UPLOAD_ALREADY_COMPLETED,
+    UNSUPPORTED_OBJECT_UPLOADED,
+    COMPLETE_MULTIPART_UPLOAD_CALLED_TOO_SOON,
+} from "../errorCodes";
+
+export const invalidMultipartNumberSchema = z.object({
+    code: z.literal(INVALID_MULTIPART_NUMBER),
+});
+
+export const mediaObjectNotFoundSchema = z.object({
+    code: z.literal(MEDIA_OBJECT_NOT_FOUND),
+});
+
+export const mediaTooLargeSchema = z
+    .object({
+        code: z.literal(MEDIA_TOO_LARGE),
+        details: z.object({
+            mediaID: z.uuidv7(),
+            currentSize: z.int(),
+            maxSupportedSize: z.int(),
+        }),
+    })
+    .transform(({ code, details }) => ({
+        code,
+        mediaID: details.mediaID,
+        currentSize: details.currentSize,
+        maxSupportedSize: details.maxSupportedSize,
+    }));
+
+export const mediaItemsLimitExceededSchema = z
+    .object({
+        code: z.literal(MEDIA_ITEMS_LIMIT_EXCEEDED),
+        details: z.object({
+            currentLength: z.int(),
+            maxItemsSupported: z.int(),
+        }),
+    })
+    .transform(({ code, details }) => ({
+        code,
+        currentLength: details.currentLength,
+        maxItemsSupported: details.maxItemsSupported,
+    }));
+
+export const uploadAlreadyCompletedSchema = z.object({
+    code: z.literal(UPLOAD_ALREADY_COMPLETED),
+});
+
+export const unsupportedObjectUploadedSchema = z.object({
+    code: z.literal(UNSUPPORTED_OBJECT_UPLOADED),
+});
+
+export const completeMultipartUploadCalledTooSoonSchema = z.object({
+    code: z.literal(COMPLETE_MULTIPART_UPLOAD_CALLED_TOO_SOON),
+});

c4b7de314f7c67cd815b9f919256b3bd9c017917
rohan <rohanverma031@gmail.com>
2026-03-13 09:37:58 +0530

    feat: added schmea for iam errors

--- src/errorCodes/iam.ts
+++ src/errorCodes/iam.ts
@@ -1,4 +1,4 @@
-export const PERMISSION_DENIED = "permission-denied";
+export const SELF_PERMISSION_MISMATCH = "self-permission-mismatch";
 export const INVALID_ACTOR = "invalid-actor";
 export const PERMISSION_EXPLICITLY_DENIED = "permission-explicitly-denied";
 export const MISSING_PERMISSION = "missing-permission";
---
+++ src/errorSchema/iam.ts
@@ -0,0 +1,63 @@
+import z from "zod";
+import {
+    SELF_PERMISSION_MISMATCH,
+    INVALID_ACTOR,
+    PERMISSION_EXPLICITLY_DENIED,
+    MISSING_PERMISSION,
+} from "../errorCodes";
+
+export const selfPermissionMismatchSchema = z
+    .object({
+        code: z.literal(SELF_PERMISSION_MISMATCH),
+        details: z.object({
+            permission: z.string(),
+            key: z.string(),
+            required: z.string(),
+            got: z.string(),
+        }),
+    })
+    .transform(({ code, details }) => ({
+        code,
+        permission: details.permission,
+        key: details.key,
+        required: details.required,
+        got: details.got,
+    }));
+
+export const invalidActorSchema = z
+    .object({
+        code: z.literal(INVALID_ACTOR),
+        details: z.object({
+            supportedActors: z.array(z.string()),
+            currentActor: z.string(),
+        }),
+    })
+    .transform(({ code, details }) => ({
+        code,
+        supportedActors: details.supportedActors,
+        currentActor: details.currentActor,
+    }));
+
+export const permissionExplicitlyDeniedSchema = z
+    .object({
+        code: z.literal(PERMISSION_EXPLICITLY_DENIED),
+        details: z.object({
+            deniedPermission: z.string(),
+        }),
+    })
+    .transform(({ code, details }) => ({
+        code,
+        deniedPermission: details.deniedPermission,
+    }));
+
+export const missingPermissionSchema = z
+    .object({
+        code: z.literal(MISSING_PERMISSION),
+        details: z.object({
+            missingPermission: z.string(),
+        }),
+    })
+    .transform(({ code, details }) => ({
+        code,
+        missingPermission: details.missingPermission,
+    }));

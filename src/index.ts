e22934c56a31ffa535c17015171c8c196b94603d
rohan <rohanverma031@gmail.com>
2026-03-15 14:35:11 +0530

    update: index.ts gen and zod import

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 // AUTO-GENERATED FILE — DO NOT EDIT
 
 export * from "./constants";
+export * from "./errorCodes";
+export * from "./errorSchema";
 export * from "./errors";
 export * from "./media";
 export * from "./response";
--- src/response/decode.ts
+++ src/response/decode.ts
@@ -1,4 +1,4 @@
-import { z } from "zod";
+import z from "zod";
 import { ApplicationError } from "../errors";
 import { serverCodes } from "../errorCodes";
 import { parseSuccessReponse } from "./successReponse";

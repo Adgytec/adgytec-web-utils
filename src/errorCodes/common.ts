d8465f3934604dada1e1e22f024dc8e0f450ea6b
rohan <rohanverma031@gmail.com>
2026-03-16 14:13:29 +0530

    Feat: added error codes for network and unexpected error

--- src/errorCodes/common.ts
+++ src/errorCodes/common.ts
@@ -2,4 +2,6 @@ export const commonCodes = {
     invalidId: "invalid-id",
     routeNotFound: "route-not-found",
     methodNotAllowed: "method-not-allowed",
+    networkError: "network-error",
+    unexpectedError: "unexpected-error",
 } as const;
